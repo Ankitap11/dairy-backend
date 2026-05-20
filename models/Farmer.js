6ab628fed4d6aa3cc76e2482b3b9012c31064e94
Ankitap11 <ankita_ap.starsoftech@outlook.com>
2026-05-20 23:55:22 +0530

    Delete models directory

--- models/Farmer.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-const FarmerSchema = new mongoose.Schema({
-    name: String,
-    phone: String
-});
-
-module.exports = mongoose.model('Farmer', FarmerSchema);
